cf600e17edd59ff9d71e09379f840bdfac23bd54
BUKOWSKI Paul <bukowski.paul@icloud.com>
2025-04-01 09:05:01 +0200

    compressing images, even the full resolution ones

--- compresser.py
+++ compresser.py
@@ -2,12 +2,21 @@ from PIL import Image
 import os
 
 input_folder = "ressources\\diving photos\\full"
-output_folder = "ressources\\diving photos\\low"
 
-new_width = 300
+output_folder = "ressources\\diving photos\\optimized"
 
 for i in os.listdir(input_folder):
 
     image = Image.open(input_folder+"\\"+i)
     
-    image.resize((new_width, image.height*new_width//image.width), Image.Resampling.LANCZOS).save(output_folder+"\\"+i)
+    image.resize((image.width, image.height), Image.Resampling.LANCZOS).save(output_folder+"\\"+i, optimize=True, quality=90)
+
+
+output_folder = "ressources\\diving photos\\low"
+new_width = 400
+
+for i in os.listdir(input_folder):
+
+    image = Image.open(input_folder+"\\"+i)
+    
+    image.resize((new_width, image.height*new_width//image.width), Image.Resampling.LANCZOS).save(output_folder+"\\"+i, optimize=True, quality=95)
